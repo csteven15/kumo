86cc9761bd6baf4202b58b6de039c9293d931450
Philip Rodriguez <philiprodriguez@knights.ucf.edu>
2018-08-30 15:39:34 -0400

    Added firebase example functionality of reading and writing database to home component

--- src/components/home.js
+++ src/components/home.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import { Component } from 'react';
 import Mapbox from './mapbox';
+import { Button } from 'reactstrap';
+import fire from '../fire';
 
-const Home = () => {
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.state.dataStuff = {};
+  }
+
+  componentWillMount() {
+    fire.database().ref('/').on('value', (snapshot) => {
+      this.state.dataStuff = snapshot;
+      this.setState(this.state);
+    });
+  }
+
+  render() {
     return (
-        <div>
-            <h1>Home Component</h1>
-            <Mapbox />
-        </div>
-    )
-};
+      <div>
+        <h1>Home Component</h1>
+        <p>
+          The data currently looks like:
+          { JSON.stringify(this.state.dataStuff) }
+        </p>
+        <Mapbox />
+        <Button color="primary" size="sm" onClick={ () => { fire.database().ref('testPushing').push({name: "Philip", lname: "Rodrigo"}); alert('pushed'); } }>Test Button</Button>
+      </div>
+    );
+  }
+}
 
 export default Home;

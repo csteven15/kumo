3e59a28608bab9eff9ddcaab645c9bdbb50e287e
Alex Coleman <alexrcoleman13@gmail.com>
2018-09-25 12:44:16 -0400

    Add logout button

--- src/components/AdminControls.js
+++ src/components/AdminControls.js
@@ -1,4 +1,6 @@
+import fire from '../fire';
 import React from 'react';
+import { Button } from 'reactstrap';
 
 type Props = {
     user: Object,
@@ -8,9 +10,13 @@ class AdminControls extends React.Component<{},Props> {
   render() {
     return (
         <div style={{ backgroundColor: '#eeeeee' }}>
+        <Button onClick={this.handleSignOut}>Logout</Button>
             Welcome {this.props.user.email} to Admin page!
         </div>
     )
+  }
+  handleSignOut = () => {
+    fire.auth().signOut();
   }
 };
 

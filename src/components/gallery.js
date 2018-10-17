b3401cb2841d228860ce95346fe7d08bb3dd39a0
Philip Rodriguez <philiprodriguez@knights.ucf.edu>
2018-10-17 14:30:53 -0400

    Gallery now supports the property "numImages" which determines the number of images the Gallery component will load and display.

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -8,14 +8,9 @@ import Fade from 'react-reveal/Fade';
 const storage = Firebase.storage().ref();
 
 const imageNames = ["FullSizeRender", "FullSizeRender 1", "drinks", "fish1", "fish2", "noodle", "popcornChicken", "shrimp", "sushi", "FullSizeRender 2", "FullSizeRender 3", "IMG_1539", "IMG_1540", "IMG_1541", "IMG_1565", "IMG_1566", "IMG_1568", "IMG_1575"];
-let imagesLoaded = [];
 
 function logFirebaseError(firebaseError) {
   console.log("An error occurred: " + firebaseError.message);
-}
-
-function allImagesLoaded() {
-  return imagesLoaded.length === imageNames.length;
 }
 
 function compareImages(a, b) {
@@ -25,24 +20,51 @@ function compareImages(a, b) {
 class Gallery extends Component {
     constructor(props) {
         super(props);
+        this.getNumberOfImagesToLoad = this.getNumberOfImagesToLoad.bind(this);
+        this.allImagesLoaded = this.allImagesLoaded.bind(this);
+        this.imagesLoaded = [];
         this.state = {
           images: []
         }
+    }
+
+    getNumberOfImagesToLoad() {
+      let imagesToLoad = this.props.numImages;
+      if (imagesToLoad == undefined || imagesToLoad == null) {
+        imagesToLoad = imageNames.length;
+      }
+      if (imagesToLoad > imageNames.length) {
+        imagesToLoad = imageNames.length;
+      }
+      return imagesToLoad;
+    }
+
+    allImagesLoaded() {
+      return this.imagesLoaded.length === this.getNumberOfImagesToLoad();
     }
 
     componentWillMount() {
       console.log("Called componentWillMount");
 
       // Did we already load the images before? (Happens if we switch out of Gallery page and then back into it)
-      if (allImagesLoaded()) {
+      if (this.allImagesLoaded()) {
         console.log("State set on reentry");
         // We are done!
-        this.setState({ images: imagesLoaded });
+        this.setState({ images: this.imagesLoaded });
       } else {
         // For each of the image names, we want to get the image's download URL
         // and then the image's thumbnail download url and width and height. On
         // the last image being loaded, we want to present the gallery.
         imageNames.forEach((element, index, array) => {
+          // We want to only load the number of images we were told to display
+          // by our props. If no prop was given, then assume loading them all is
+          // the intended outcome.
+
+          if (index >= this.getNumberOfImagesToLoad()) {
+            console.log("Not loading image with index " + index + " since props instructed the component to only load " + this.getNumberOfImagesToLoad() + " images.");
+            return;
+          }
+
           storage.child("menu").child(element + ".jpg").getDownloadURL().then((primaryURL) => {
             console.log("Got primaryURL for " + element);
             storage.child("menu").child(element + "_thumbnail.jpg").getDownloadURL().then((thumbnailURL) => {
@@ -53,7 +75,7 @@ class Gallery extends Component {
                 console.log("Thumbnail prefetched for " + element);
                 // Now we know thumbnail width and height, we can construct our
                 // imagesLoaded array.
-                imagesLoaded.push(
+                this.imagesLoaded.push(
                   {
                     sortOrder: index,
                     src: primaryURL,
@@ -65,12 +87,12 @@ class Gallery extends Component {
 
                 // If we just finished loading the last image, then put it into
                 // our state so we refresh the view!
-                console.log("imagesLoaded at " + imagesLoaded.length);
-                if (allImagesLoaded()) {
+                console.log("imagesLoaded at " + this.imagesLoaded.length);
+                if (this.allImagesLoaded()) {
                   console.log("State set on " + element);
-                  imagesLoaded.sort(compareImages);
+                  this.imagesLoaded.sort(compareImages);
                   // We are done!
-                  this.setState({ images: imagesLoaded });
+                  this.setState({ images: this.imagesLoaded });
                 }
               };
             }).catch(logFirebaseError);
@@ -80,8 +102,8 @@ class Gallery extends Component {
     }
 
     render() {
-      if (allImagesLoaded()) {
-        console.log("All images loaded rendering!");
+      if (this.allImagesLoaded()) {
+        console.log("Final gallery is rendering!");
         return (
           <div className="gallery">
 
@@ -90,7 +112,7 @@ class Gallery extends Component {
           </div>
         );
       } else {
-        console.log("Loading rendering!");
+        console.log("Loading message is rendering!");
         return (
           <div className="gallery">
             <Fade big>
